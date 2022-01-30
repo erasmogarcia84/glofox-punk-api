37d9abd5ea1502dd1457eb7916dd79c7cb9d284f
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-30 23:45:28 +0100

    Connect randomBeer component to store

--- src/components/RandomBeer/index.jsx
+++ src/components/RandomBeer/index.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getRandomBeer } from '../../redux/reducers/randomBeerReducer';
 
-const randomBeer = {
-  id: 192,
-  name: 'Punk IPA 2007 - 2010',
-  tagline: 'Post Modern Classic. Spiky. Tropical. Hoppy.',
-  first_brewed: '04/2007',
-  description:
-    "Our flagship beer that kick started the craft beer revolution. This is James and Martin's original take on an American IPA, subverted with punchy New Zealand hops. Layered with new world hops to create an all-out riot of grapefruit, pineapple and lychee before a spiky, mouth-puckering bitter finish.",
-  image_url: 'https://images.punkapi.com/v2/192.png',
-  abv: 6.0,
-  ibu: 60.0,
+const RandomBeer = () => {
+  const dispatch = useDispatch();
+  const randomBeer = useSelector((state) => state.randomBeer);
+
+  const handleRandomBeer = () => {
+    dispatch(getRandomBeer());
+  };
+
+  return (
+    <section id="randomBeer">
+      <img src={randomBeer.image_url} alt={randomBeer.name} />
+      <h2>{randomBeer.name}</h2>
+      <p>{randomBeer.description}</p>
+      <button type="button" onClick={handleRandomBeer}>
+        Another Beer
+      </button>
+      <button type="button">Random Non Alcoholic Beer</button>
+    </section>
+  );
 };
-
-const RandomBeer = () => (
-  <section id="randomBeer">
-    <img src={randomBeer.image_url} alt={randomBeer.name} />
-    <h2>{randomBeer.name}</h2>
-    <p>{randomBeer.description}</p>
-    <button type="button">Another Beer</button>
-    <button type="button">Random Non Alcoholic Beer</button>
-  </section>
-);
 
 export default RandomBeer;
