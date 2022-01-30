af7c5d835e373012198613890620788bc0dcb8ec
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-30 23:58:06 +0100

    Add getRandomNonAlcoholicBeer action

--- src/redux/reducers/randomBeerReducer.js
+++ src/redux/reducers/randomBeerReducer.js
@@ -2,16 +2,20 @@
 
 import axios from 'axios';
 
-const URL = 'https://api.punkapi.com/v2/beers/random';
+const BASE_URL = 'https://api.punkapi.com/v2/beers';
+const MIN_ABV = 0.51;
 
 export const actions = {
   GET_RANDOM_BEER: 'randomBeer/GET_RANDOM_BEER',
   GET_RANDOM_BEER_ERROR: 'randomBeer/GET_RANDOM_BEER_ERROR',
+  GET_NON_ALCOHOLIC_RANDOM_BEER: 'randomBeer/GET_NON_ALCOHOLIC_RANDOM_BEER',
+  GET_NON_ALCOHOLIC_RANDOM_BEER_ERROR:
+    'randomBeer/GET_NON_ALCOHOLIC_RANDOM_BEER_ERROR',
 };
 
 export const getRandomBeer = () => async (dispatch) => {
   try {
-    const { data } = await axios(URL);
+    const { data } = await axios(`${BASE_URL}/random`);
     const randomBeer = data[0];
 
     dispatch({
@@ -26,9 +30,27 @@ export const getRandomBeer = () => async (dispatch) => {
   }
 };
 
+export const getRandomNonAlcoholicBeer = () => async (dispatch) => {
+  try {
+    const { data } = await axios(`${BASE_URL}?abv_lt=${MIN_ABV}`);
+    const randomBeer = data[0];
+
+    dispatch({
+      type: actions.GET_NON_ALCOHOLIC_RANDOM_BEER,
+      randomBeer,
+    });
+  } catch (error) {
+    dispatch({
+      type: actions.GET_NON_ALCOHOLIC_RANDOM_BEER_ERROR,
+      message: error.message,
+    });
+  }
+};
+
 export default function randomBeerReducer(randomBeer = null, action) {
   switch (action.type) {
     case actions.GET_RANDOM_BEER:
+    case actions.GET_NON_ALCOHOLIC_RANDOM_BEER:
       return action.randomBeer;
 
     default:
