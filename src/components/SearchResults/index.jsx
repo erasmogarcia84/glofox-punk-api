ad08a83fa1bc2c82e3667fbf55fbbae6ab8b2f49
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-31 01:57:00 +0100

    Show beer list with cards

--- src/components/SearchResults/index.jsx
+++ src/components/SearchResults/index.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import imagePlaceholder from '../../assets/images/placeholder.png';
 
-const SearchResults = () => (
-  <section id="searchResults">
-    <h2>Search Results</h2>
-  </section>
-);
+const SearchResults = () => {
+  const beers = useSelector((state) => state.beers);
+
+  return (
+    beers?.length > 0 && (
+      <section id="searchResults">
+        <h2>Search Results</h2>
+        <p>(Found {beers.length} beers)</p>
+        <ul>
+          {beers?.map((beer) => (
+            <li className="card" key={beer.id}>
+              <div className="card__image">
+                <img
+                  src={beer.image_url || imagePlaceholder}
+                  alt={beer.name}
+                  title={beer.name}
+                />
+                <div className="card__title">
+                  <span className="card__title--dark">{beer.name}</span>
+                  <span className="card__title--light">{beer.tagline}</span>
+                </div>
+              </div>
+              <p>{beer.description}</p>
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+  );
+};
 
 export default SearchResults;
