1cdf0c38913330b5dcf7a0a25083a4ade510930c
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-31 01:11:59 +0100

    Connect SearchBox to redux

--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,21 +1,49 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { getBeers } from '../../redux/reducers/beersReducer';
+import './searchBox.scss';
+
 import filters from './constants';
 
 const SearchBox = () => {
+  const dispatch = useDispatch();
   const [searchFilter, setSearchFilter] = useState(filters.ByName);
+  const [searchValue, setSearchValue] = useState('');
+
   const handleFilter = (filter) => {
     setSearchFilter(filter);
   };
+
+  const handleGetBeersList = () => {
+    dispatch(getBeers(searchFilter, searchValue));
+  };
+
   return (
     <section id="searchBeer">
       <h2>Search</h2>
       <div className="searchBeer__bar">
         {searchFilter === filters.ByName ? (
-          <input type="text" placeholder="write something..." />
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(event) => setSearchValue(event.target.value)}
+            placeholder="write something..."
+          />
         ) : (
-          <input type="date" />
+          <input
+            type="date"
+            value={searchValue}
+            onChange={(event) => setSearchValue(event.target.value)}
+          />
         )}
-        <button type="button">Search</button>
+
+        <button
+          type="button"
+          onClick={handleGetBeersList}
+          disabled={!searchFilter || searchValue.length < 3}
+        >
+          Search
+        </button>
       </div>
       <div className="searchBeer__options">
         <div>
