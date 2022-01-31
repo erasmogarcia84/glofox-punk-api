4bd0398cd1b5599c09bde36fb0280313b4ef312e
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-31 01:35:11 +0100

    Create SearchResults component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import RandomBeer from './components/RandomBeer';
 import SearchBox from './components/SearchBox';
+import SearchResults from './components/SearchResults';
 
 const App = () => (
   <>
     <RandomBeer />
     <SearchBox />
+    <SearchResults />
   </>
 );
 
---
+++ src/components/SearchResults/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchResults = () => (
+  <section id="searchResults">
+    <h2>Search Results</h2>
+  </section>
+);
+
+export default SearchResults;
