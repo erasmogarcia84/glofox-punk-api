d04f715e5598fb71c9e99878c5cc441ed1f709fc
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-31 02:50:30 +0100

    Create Footer component

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import logoGithub from '../../assets/images/logo-github.svg';
+import logoLinkedin from '../../assets/images/logo-linkedin.svg';
+
+const Footer = () => (
+  <footer>
+    <a
+      href="https://github.com/erasmogarcia84/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img src={logoGithub} alt="Github logo" />
+    </a>
+    <a
+      href="https://www.linkedin.com/in/erasmogarciaglez/"
+      target="_blank"
+      rel="noreferrer"
+    >
+      <img src={logoLinkedin} alt="Linkedin logo" />
+    </a>
+  </footer>
+);
+
+export default Footer;
