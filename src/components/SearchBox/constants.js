82efc1aa4fe8a265d72ca242cfa2eba470a6b637
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-01-31 01:02:09 +0100

    Handle filters for toggle input

---
+++ src/components/SearchBox/constants.js
@@ -0,0 +1,6 @@
+const filters = {
+  ByName: 'beer_name',
+  ByBrewedBefore: 'brewed_before',
+};
+
+export default filters;
--- src/components/SearchBox/index.jsx
+++ src/components/SearchBox/index.jsx
@@ -1,27 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
+import filters from './constants';
 
-const SearchBox = () => (
-  <section id="searchBeer">
-    <h2>Search</h2>
-    <div className="searchBeer__bar">
-      <input type="text" placeholder="write something..." />
-      <button type="button">Search</button>
-    </div>
-    <div className="searchBeer__options">
-      <div>
-        <label htmlFor="searchFilter">
-          <input type="radio" name="searchForm" id="searchFilter" />
-          By Name
-        </label>
+const SearchBox = () => {
+  const [searchFilter, setSearchFilter] = useState(filters.ByName);
+  const handleFilter = (filter) => {
+    setSearchFilter(filter);
+  };
+  return (
+    <section id="searchBeer">
+      <h2>Search</h2>
+      <div className="searchBeer__bar">
+        {searchFilter === filters.ByName ? (
+          <input type="text" placeholder="write something..." />
+        ) : (
+          <input type="date" />
+        )}
+        <button type="button">Search</button>
       </div>
-      <div>
-        <label htmlFor="searchValue">
-          <input type="radio" name="searchForm" id="searchValue" />
-          By Brewed Before
-        </label>
+      <div className="searchBeer__options">
+        <div>
+          <label htmlFor="searchFilter">
+            <input
+              type="radio"
+              name="searchForm"
+              id="searchFilter"
+              value={filters.ByName}
+              onChange={(event) => handleFilter(event.target.value)}
+            />
+            By Name
+          </label>
+        </div>
+        <div>
+          <label htmlFor="searchValue">
+            <input
+              type="radio"
+              name="searchForm"
+              id="searchValue"
+              value={filters.ByBrewedBefore}
+              onChange={(event) => handleFilter(event.target.value)}
+            />
+            By Brewed Before
+          </label>
+        </div>
       </div>
-    </div>
-  </section>
-);
-
+    </section>
+  );
+};
 export default SearchBox;
