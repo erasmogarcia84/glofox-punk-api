9b0fb9472cf03c508c1ac043bdd46d0b4d89cdc0
erasmogarcia84 <erasmogarciaglez@gmail.com>
2022-02-02 02:37:26 +0100

    Refactor formatDate function

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,12 +1,8 @@
 export const getRandomFromArray = (beers, prevBeer) => {
   const withoutPrevBeer = beers.filter((beer) => beer.id !== prevBeer.id);
   const randomIndex = Math.floor(Math.random() * withoutPrevBeer.length);
-
   return withoutPrevBeer[randomIndex];
 };
 
-export const formatDate = (initialDate) => {
-  const newDate = initialDate.split('-').reverse().join('-').substr(3);
-
-  return newDate;
-};
+export const formatDate = (initialDate) =>
+  initialDate.split('-').reverse().join('-').substr(3);
